ed12bbbf3786afaf2b36bb3e95ecd7bfdfa2ae86
quix10 <sergey.mullin@gmail.com>
2020-12-24 10:18:03 +0300

    implemented guessing game

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,7 +1,24 @@
-0
-1
-2
-3
-4
-5
-6
+#!/usr/bin/env bash
+
+response=0
+
+function giveAdvise {
+	if [[ $1 -lt $2 ]]
+	then
+		echo "The answer is too low. Please try again."
+	elif [[ $1 -gt $2 ]]
+	then
+		echo "The answer is too hight. Please try again."
+	else
+		echo "Congradulations! The answer is correct!"
+	fi
+}
+
+correctNumberOfFiles=$(($(ls -l | wc -l)-1))
+
+while [[ $response -ne $correctNumberOfFiles ]]
+do
+	echo "How many files are in the current directory? Please enter a number:"
+	read response
+	giveAdvise $response $correctNumberOfFiles
+done
